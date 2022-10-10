dbbb2f6bcfc8e503f2fce489f6314147ae804331
Sachini Kandawala <sach7691@gmail.com>
2022-10-10 16:11:34 +0000

    done up to conditional if

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-print('Enter your name')
-x = input()
-
-print('Hello', x)
+# print('Enter your name')
+# x = input()
+# print('Hello', x)
+# ---------------------------------
+# x = int (input('Enter your x :'))
+# y = int (input('Enter your y :'))
+# z = x + y
+# print('sum is :', z)
+# ---------------------------------
+# my_list = ['sach',100,True]
+# print(my_list)
+# print(my_list[0]) 
+# print(my_list[-3])
+# both print values give same output = sach
+# ---------------------------------
+# you can extend list using append() , list is mutable
+# my_list = ['sach',100,True]
+# my_list.append(20.3) 
+# print(my_list)
+# -----------------------------------
+# you cannot extend tuple using append() because it is immutable ,but through casting you can
+# my_tuple = ('sach',100,True)
+# my_list = list(my_tuple)
+# my_list.append(20.3) 
+# print(my_list )
+# -----------------------------------
+# a = 130
+# b = 17
+# if a > b :
+#   print("a is greater")
+# -----------------------------------
+ 
